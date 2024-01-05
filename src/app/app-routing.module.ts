068ae62348232de165a42adc5acf4b8ae98c8066
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2024-01-05 22:29:53 +0100

    Created base layout of the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { PageContentComponent } from './page-content/page-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PageContentComponent },
+  { path: 'subpage', component: PageContentComponent },
+  { path: 'about', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageContentComponent } from './page-content/page-content.component';
+import { PageInfoComponent } from './page-info/page-info.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    PageContentComponent,
+    PageInfoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
